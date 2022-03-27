ef047a03c60073920a80d2c72e92e55047a78802
김문수 <56672937+anstn1993@users.noreply.github.com>
2022-03-27 17:25:41 +0900

    Time: 0 ms (100.00%), Space: 43.4 MB (57.93%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        return search(nums, target, 0, nums.length - 1);
+    }
+    
+    private int search(int[] nums, int target, int startIndex, int endIndex) {
+        if(startIndex > endIndex) {
+            return startIndex;
+        }
+        
+        int midIndex = (startIndex + endIndex) / 2;
+        
+        if(target == nums[midIndex]) {
+            return midIndex;
+        } 
+        else if(target > nums[midIndex]) {
+            return search(nums, target, midIndex + 1, endIndex);
+        }
+        else {
+            return search(nums, target, startIndex, midIndex - 1);
+        }
+    }
+}
