adb6f768d56727475556dd49d3e740803639c34e
김문수 <56672937+anstn1993@users.noreply.github.com>
2024-02-11 18:32:20 +0900

    Time: 0 ms (100.00%), Space: 45.5 MB (23.14%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            result = result ^ i;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        
+        return result;
+    }
+}
